755eea1ced480091e21ac5fb72fdd54f03a06f30
Georg Boegerl <georg.boegerl@web.de>
2016-04-01 21:41:08 +0200

    refactor

--- DocumentsApp/src/com/rembli/ums/UserManagementSystem.java
+++ DocumentsApp/src/com/rembli/ums/UserManagementSystem.java
@@ -18,10 +18,91 @@ public class UserManagementSystem {
 		public static String FACEBOOK = "FACEBOOK";
 	}	
 	
+	// ### METHODS FOR MANAGEMENT ACCESS TOKEBN #################################	  	
+	
 	// die Tokens werden Anwendungsübergreifend in einer Hashmap abgespeichert, d.h. pro VM bzw. App-Server
 	// Alternative: in der Datenbank
+	
 	private static HashMap<String,AccessToken> accessTokens = new HashMap<String,AccessToken>();
     
+    private String issueAccessToken (String username) throws Exception {
+    	// Es kann ein beliebiges Token erzeugt werden, z.B. auch ein JWT. Hier nur eine Zufallszahl
+    	// Vorteil von JWT: Ein System, welches den öffentlichen Schlüssel hat, kann beim JWT
+    	// verifizieren, von wem es erzeugt wurde und dann - wenn es dem Aussteller vertraut - ein SSO durchführen
+    	// siehe auch JWS - JSON Web Signature
+    	
+    	Random random = new SecureRandom();
+        String accessTokenSignature = "Bearer " + new BigInteger(130, random).toString(32);
+        int secondsToLive = new Integer(getProperties ("SEC.TOKEN_VALIDTY_IN_SECONDS")).intValue(); // Gültigkeit des Tokens in Sekunden
+        AccessToken accessToken = new AccessToken (username, secondsToLive);
+        accessTokens.put(accessTokenSignature, accessToken);
+
+		return accessTokenSignature;
+    }	
+    
+    public java.util.Date getAccessTokenExipration (String accessTokenSignature) throws Exception {
+    	AccessToken accessToken = accessTokens.get(accessTokenSignature);
+    	return accessToken.getExpiresOn();
+    }    
+        
+    public void refreshAccessToken (String accessTokenSignature) {
+		try {    	
+	        int secondsToLive = new Integer(getProperties ("SEC.TOKEN_VALIDTY_IN_SECONDS")).intValue();    	
+	    	AccessToken accessToken = accessTokens.get (accessTokenSignature);
+			java.util.Date now = new java.util.Date ();
+			now.setTime(now.getTime() + (secondsToLive * 1000));    
+			accessToken.setExpiresOn(now);
+	    	accessTokens.put(accessTokenSignature, accessToken);
+
+		} catch (IOException ignored) {
+			ignored.printStackTrace();
+		}
+    }
+    
+    public void revokeAccessToken (String accessTokenSignature) {
+    	accessTokens.remove(accessTokenSignature);
+    }
+    
+    public boolean isAuthenticated (String accessTokenSignature) {
+    	AccessToken accessToken = accessTokens.get(accessTokenSignature);
+    	java.util.Date now = new java.util.Date();
+    	if (accessToken.getExpiresOn().getTime()>now.getTime())
+    		return true;
+    	else 
+    		return false;
+    }    
+
+    public String getUsername (String accessTokenSignature) {
+    	AccessToken accessToken = accessTokens.get(accessTokenSignature);
+    	return accessToken.getUsername();
+    }	
+	
+	// ### PUBLIC METHODS ##################################
+	
+	public long createUserInfo (String username, String email, String password) throws Exception {
+		try (Connection con = ConnectionPool.getConnection()) {
+			// Prüfen, ob es den username schon gibt
+    		String sql = SqlStatements.get ("UMS.CHECK_USERINFO");
+    		Table t = con.createQuery(sql)
+				.addParameter("username", username)
+				.executeAndFetchTable();
+    		
+    		if (t.rows().size()>0) return 0;			
+			
+    		// wenn nicht, dann entsprechend anlegen
+			sql = SqlStatements.get ("UMS.CREATE_USERINFO");
+		    Long id = con.createQuery(sql, true)
+		    		.addParameter("username", username)
+		    		.addParameter("email", email)
+		    		.addParameter("password", password)		    		
+				    .executeUpdate()
+				    .getKey(Long.class);
+		    LogManagementSystem.log(username, LogEntry.ENTITY.USER, username, LogEntry.ACTION.CREATE, "User "+ username+" created");
+		    
+		    return id.longValue();
+		}
+	} 	
+	
     public String login (String username, String password) throws Exception {
     	if (username==null || username.length()==0 || password==null || password.length()==0)
     		throw new NotAuthorizedException("Username/password wrong");
@@ -85,110 +166,37 @@ public class UserManagementSystem {
     	else
     		throw new NotAuthorizedException(identityProvider + " not supported.");
     }
-    
-    private String issueAccessToken (String username) throws Exception {
-    	// Es kann ein beliebiges Token erzeugt werden, z.B. auch ein JWT. Hier nur eine Zufallszahl
-    	// Vorteil von JWT: Ein System, welches den öffentlichen Schlüssel hat, kann beim JWT
-    	// verifizieren, von wem es erzeugt wurde und dann - wenn es dem Aussteller vertraut - ein SSO durchführen
-    	// siehe auch JWS - JSON Web Signature
-    	
-    	Random random = new SecureRandom();
-        String accessTokenSignature = "Bearer " + new BigInteger(130, random).toString(32);
-        int secondsToLive = new Integer(getProperties ("SEC.TOKEN_VALIDTY_IN_SECONDS")).intValue(); // Gültigkeit des Tokens in Sekunden
-        AccessToken accessToken = new AccessToken (username, secondsToLive);
-        accessTokens.put(accessTokenSignature, accessToken);
-
-		return accessTokenSignature;
-    }	
-    
-    public java.util.Date getAccessTokenExipration (String accessTokenSignature) throws Exception {
-    	AccessToken accessToken = accessTokens.get(accessTokenSignature);
-    	return accessToken.getExpiresOn();
-    }    
-        
-    public void refreshAccessToken (String accessTokenSignature) {
-		try {    	
-	        int secondsToLive = new Integer(getProperties ("SEC.TOKEN_VALIDTY_IN_SECONDS")).intValue();    	
-	    	AccessToken accessToken = accessTokens.get (accessTokenSignature);
-			java.util.Date now = new java.util.Date ();
-			now.setTime(now.getTime() + (secondsToLive * 1000));    
-			accessToken.setExpiresOn(now);
-	    	accessTokens.put(accessTokenSignature, accessToken);
-
-		} catch (IOException ignored) {
-			ignored.printStackTrace();
-		}
-    }
-    
-    public void revokeAccessToken (String accessTokenSignature) {
-    	accessTokens.remove(accessTokenSignature);
-    }
-    
-    public boolean isAuthenticated (String accessTokenSignature) {
-    	AccessToken accessToken = accessTokens.get(accessTokenSignature);
-    	java.util.Date now = new java.util.Date();
-    	if (accessToken.getExpiresOn().getTime()>now.getTime())
-    		return true;
-    	else 
-    		return false;
-    }    
-
-    public String getUsername (String accessTokenSignature) {
-    	AccessToken accessToken = accessTokens.get(accessTokenSignature);
-    	return accessToken.getUsername();
-    }
 	
-	public long createUserInfo (String username, String email, String password) throws Exception {
-		System.out.println("Try to create new user: "+username);
-		try (Connection con = ConnectionPool.getConnection()) {
-			// Prüfen, ob es den username schon gibt
-    		String sql = SqlStatements.get ("UMS.CHECK_USERINFO");
-    		Table t = con.createQuery(sql)
-				.addParameter("username", username)
-				.executeAndFetchTable();
-    		
-    		if (t.rows().size()>0) return 0;			
-			
-    		// wenn nicht, dann entsprechend anlegen
-			sql = SqlStatements.get ("UMS.CREATE_USERINFO");
-		    Long id = con.createQuery(sql, true)
-		    		.addParameter("username", username)
-		    		.addParameter("email", email)
-		    		.addParameter("password", password)		    		
-				    .executeUpdate()
-				    .getKey(Long.class);
-		    LogManagementSystem.log(username, LogEntry.ENTITY.USER, username, LogEntry.ACTION.CREATE, "User "+ username+" created");
-		    
-		    return id.longValue();
-		}
-	}    
+	// ### METHODS ONLY ACCESSIBLE WITH VALID ACCESS TOKEN ###########################
 	
     public UserInfo getUserInfo (String accessTokenSignature) throws Exception {
-    	AccessToken accessToken = accessTokens.get(accessTokenSignature);
-    	System.out.println ("Username (Token): "+accessToken.getUsername());
-
+    	if (!isAuthenticated (accessTokenSignature))
+    		throw new NotAuthorizedException ("Valid access token required.");
+    	
 		try (Connection con = ConnectionPool.getConnection()) {    	
 	   		String sql = SqlStatements.get ("UMS.GET_USERINFO");
 	   		return con.createQuery(sql)
-	   				.addParameter("username", accessToken.getUsername())
+	   				.addParameter("username", getUsername(accessTokenSignature))
 	   				.executeAndFetch(UserInfo.class)
 	   				.get(0);  
 		}
     }	
 	
 	public void changePassword (String accessTokenSignature, String password) throws Exception {
-    	AccessToken accessToken = accessTokens.get(accessTokenSignature);
-    	if (accessToken==null) throw new Exception ("UNAUTHORIZED");
+    	if (!isAuthenticated (accessTokenSignature))
+    		throw new NotAuthorizedException ("Valid access token required.");
 
 		try (Connection con = ConnectionPool.getConnection()) {
 			String sql = SqlStatements.get ("UMS.UPDATE_PASSWORD");
 		    con.createQuery(sql,true)
-    				.addParameter("username", accessToken.getUsername())		    
+    				.addParameter("username", getUsername(accessTokenSignature))		    
 		    		.addParameter("password", password)
 				    .executeUpdate();
 		}		
 	}  	
 
+	// ### HELPER ####################################################################
+	
 	private String getProperties (String statement) throws IOException {
 		Properties prop = new Properties();
 	    prop.load(SqlStatements.class.getClassLoader().getResourceAsStream("sec.properties"));
