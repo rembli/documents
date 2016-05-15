460cbb4f69cf9f42624a4afb6ecc4b110c05fd83
Georg Boegerl <georg.boegerl@web.de>
2016-05-16 01:20:28 +0200

    Added Swagger Config for SSL support

---
+++ DocumentsApp/src/com/rembli/api/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.rembli.api;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import io.swagger.jaxrs.config.BeanConfig;
+
+public class SwaggerConfig extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setTitle("A simple API for documents");
+        beanConfig.setVersion("1");
+        beanConfig.setSchemes(new String[]{"https"});
+        beanConfig.setBasePath("/documents/api");
+        beanConfig.setResourcePackage("com.rembli.api");
+        beanConfig.setPrettyPrint(true);
+        beanConfig.setScan(true);
+    }
+}
